ebfef41e5f4bf75bd744f79b62948c26bb747335
DESKTOP-AL1M89N <kumarbhattarai666@gmail.com>
2024-07-17 23:35:29 +0545

    saving, adding and removing lists

--- script.js
+++ script.js
@@ -8,6 +8,28 @@ function Add(){
         let li=document.createElement("li");
         li.innerHTML=text.value;
         list.appendChild(li);
+        saveData();
         text.value='';
+        let span= document.createElement("span")
+        span.innerHTML="\u00d7"
+        li.appendChild(span)
+        saveData();
     }
 }
+list.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("first");
+        saveData()
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+})
+function saveData(){
+    localStorage.setItem("Data",list.innerHTML);
+}
+function showData(){
+    list.innerHTML=localStorage.getItem("Data")
+}
+showData()
